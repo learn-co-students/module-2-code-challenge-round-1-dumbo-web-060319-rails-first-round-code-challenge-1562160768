6d0b40aa4fae8fff060dfbf9160f0f95042ac505
Francis Mendez <fmendez884@gmail.com>
2019-07-03 11:15:11 -0400

    done

--- app/controllers/sweets_controller.rb
+++ app/controllers/sweets_controller.rb
@@ -1,5 +1,47 @@
 class SweetsController < ApplicationController
+
   def index
     @sweets = Sweet.all
   end
+
+  def new
+    @sweet = Sweet.new
+  end 
+
+  def create
+    @sweet = Sweet.create(sweet_params)
+
+    redirect_to @sweet 
+  end 
+
+  def show
+    find_sweet
+  end 
+
+  def edit
+    find_sweet
+  end 
+
+  def update
+    @sweet = Sweet.update(sweets_params)
+
+    redirect_to @sweet 
+  end 
+
+  def destroy
+    find_sweet.destroy
+  end 
+
+  def find_sweet
+    @sweet = Sweet.find(params[:id])
+  end 
+
+  private
+
+  def sweets_params
+    params.require(:sweet).permit(
+      :name
+    )
+  end
+
 end

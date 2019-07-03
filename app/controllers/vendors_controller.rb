45cb61896303645c49019b5f3c1509282d2795e6
Gia Edgington <giaedgington@Erics-MacBook.local>
2019-07-03 11:16:03 -0400

    TEST

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,7 +1,18 @@
 class VendorsController < ApplicationController
+  before_action :find_vendor, only: [:show]
 
   def index
     @vendors = Vendor.all
+  end
+
+  def show
+    @vendor_sweets = @vendor.vendor_sweets
+  end
+
+
+  private
+  def find_vendor
+    @vendor = Vendor.find(params[:id])
   end
 
 end

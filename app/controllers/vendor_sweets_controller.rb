18cc7a66c1065a3f138d201a6180a494e96fddc9
Alexander Schelchere <ascellie@gmail.com>
2019-07-03 11:14:18 -0400

    not finished

--- app/controllers/vendor_sweets_controller.rb
+++ app/controllers/vendor_sweets_controller.rb
@@ -1,2 +1,35 @@
 class VendorSweetsController < ApplicationController
+
+
+    def show
+        vendor_sweet
+    end
+
+    def new
+        new_vendor_sweet
+    end
+
+    def create
+        if @vendor_sweet = VendorSweet.new(vendor_sweet_params)
+            @vendor_sweet.save
+            redirect_to vendor_sweet_path(@vendor_sweet)
+        else
+            redirect_to new_vendor_sweet_path
+        end
+    end
+
+    private
+
+    def vendor_sweet
+        @vendor_sweet = VendorSweet.find(params[:id])
+    end
+
+    def new_vendor_sweet
+        @vendor_sweet = VendorSweet.new
+    end 
+
+    def vendor_sweet_params
+        params.require(:vendor_sweet).permit(:price, :vendor_id, :sweet_id)
+    end
+
 end
--- app/models/vendor_sweet.rb
+++ app/models/vendor_sweet.rb
@@ -1,4 +1,11 @@
 class VendorSweet < ApplicationRecord
   belongs_to :vendor
   belongs_to :sweet
+
+  validates :price, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  def full_stats
+    "#{self.sweet.name} costs $#{self.price}.00 here at the #{self.vendor.name}!"
+  end
+
 end
